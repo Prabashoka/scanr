49b38fa1c5f2af657029003b4c8f44ba43a97b26
Prabashoka <smashoka123@gmail.com>
2026-07-27 16:50:10 +1000

    removing build files

--- src/rust/target/release/build/paste-7450501a4c964920/build_script_build-7450501a4c964920.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aseelappumud\scanr\src\rust\target\release\build\paste-7450501a4c964920\build_script_build-7450501a4c964920.d: C:\Users\aseelappumud\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\paste-1.0.15\build.rs
-
-C:\Users\aseelappumud\scanr\src\rust\target\release\build\paste-7450501a4c964920\build_script_build-7450501a4c964920.exe: C:\Users\aseelappumud\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\paste-1.0.15\build.rs
-
-C:\Users\aseelappumud\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\paste-1.0.15\build.rs:
--- src/rust/target/release/build/serde-1524bebdda2f4b13/build_script_build-1524bebdda2f4b13.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aseelappumud\scanr\src\rust\target\release\build\serde-1524bebdda2f4b13\build_script_build-1524bebdda2f4b13.d: C:\Users\aseelappumud\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-C:\Users\aseelappumud\scanr\src\rust\target\release\build\serde-1524bebdda2f4b13\build_script_build-1524bebdda2f4b13.exe: C:\Users\aseelappumud\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-C:\Users\aseelappumud\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs:
