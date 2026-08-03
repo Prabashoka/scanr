40fb8bbeb6fcb39c370d7e52f1a203276d03e900
Patricia Menendez <patricia.menendez@unimelb.edu.au>
2026-08-03 13:13:40 +1000

    Adding license to cargo and rust

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,3 +1,23 @@
+
+// Copyright (C) 2026 Ashoka Prabbashwara, Patricia Menéndez, Liam Hodgkinson and Stuart Lee 
+//
+// This file is part of scanr.
+//
+// scanr is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// See the LICENSE file in the project root for the full licence text.
+
+//! Rust backend for the R `scanr` package.
+//!
+//! The language-neutral implementation lives in the `scan-core` crate.
+
+pub mod r_api;
+
+pub use r_api::*;
+
 //! Rust backend for the R `scanr` package.
 //!
 //! The language-neutral implementation lives in the `scan-core` crate.
