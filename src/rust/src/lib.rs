a4fa59fdb17e73455e0aa48adbabded44dc61ea5
Prabashoka <smashoka123@gmail.com>
2026-08-19 21:46:07 +1000

    updates to the package

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -17,11 +17,3 @@
 pub mod r_api;
 
 pub use r_api::*;
-
-//! Rust backend for the R `scanr` package.
-//!
-//! The language-neutral implementation lives in the `scan-core` crate.
-
-pub mod r_api;
-
-pub use r_api::*;
